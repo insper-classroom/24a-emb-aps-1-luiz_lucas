5fc56259c6c2da09b009eedceb42d637ed332763
luiz <luiz@Ubuntu.myguest.virtualbox.org>
2024-02-29 13:06:49 -0300

    a

--- main/main.c
+++ main/main.c
@@ -6,6 +6,9 @@ const int BTN_PIN_R = 20;
 const int BTN_PIN_G = 26;
 const int BTN_PIN_B = 18;
 const int BTN_PIN_Y = 22;
+const int BTN_PIN_T = 28;
+
+const double jit=10;
 
 const int LED_PIN_R = 8;
 const int LED_PIN_G = 14;
@@ -18,6 +21,7 @@ volatile int r=0;
 volatile int g=0;
 volatile int b=0;
 volatile int y=0;
+volatile int t=0;
 
 void pin_init(void);
 
@@ -32,11 +36,30 @@ void som(int freq, int tempo, int pino){
   
 }
 
+// int random (int n) {
+//    int i;
+//    time_t t;
+//    int *vetor = (int *)malloc(n * sizeof(int));
+   
+//    /* Intializes random number generator */
+//    srand((unsigned) time(&t));
+
+//    /* Print 5 random numbers from 0 to 49 */
+//    for( i = 0 ; i < n ; i++ ) {
+//       //printf("%d\n", rand() % 4);
+//       vetor[i]=rand() % 4;
+//    }
+   
+//    return(vetor);
+// }
+
 void btn_callback(uint gpio, uint32_t events) {
   if (gpio == BTN_PIN_R) r=1;
   if (gpio == BTN_PIN_G) g=1;
   if (gpio == BTN_PIN_B) b=1;
   if (gpio == BTN_PIN_Y) y=1;
+  if (gpio == BTN_PIN_T) t=1;
+
 }
 
 int main() {
@@ -47,9 +70,12 @@ int main() {
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
   gpio_set_irq_enabled(BTN_PIN_B, GPIO_IRQ_EDGE_FALL, true);
   gpio_set_irq_enabled(BTN_PIN_Y, GPIO_IRQ_EDGE_FALL, true);
+  gpio_set_irq_enabled(BTN_PIN_T, GPIO_IRQ_EDGE_RISE, true);
+
 
   while (true) {
     if (r){
+      busy_wait_ms(jit);
       printf("apertouR\n");
       gpio_put(LED_PIN_R, 1);
       som(1900, 300, BUZZPIN);
@@ -57,6 +83,7 @@ int main() {
       r=0;
     }
     if (g){
+      busy_wait_ms(jit);
       printf("apertouG\n");
       gpio_put(LED_PIN_G, 1);
       som(2000, 300, BUZZPIN);
@@ -64,6 +91,7 @@ int main() {
       g=0;
     }
     if (b){
+      busy_wait_ms(jit);
       printf("apertouB\n");
       gpio_put(LED_PIN_B, 1);
       som(2100, 300, BUZZPIN);
@@ -71,11 +99,27 @@ int main() {
       b=0;
     }
     if (y){
+      busy_wait_ms(jit);
       printf("apertouY\n");
       gpio_put(LED_PIN_Y, 1);
       som(2200, 300, BUZZPIN);
       gpio_put(LED_PIN_Y, 0);
       y=0;
+    }
+    if (t){
+      busy_wait_ms(jit);
+      printf("apertouT\n");
+      int a;
+      int n=4;
+      //for( int i = 0 ; i < n ; i++ ) {
+        //printf("%d\n", rand() % 4);
+      //}
+      a=rand() % 4;
+      t=0;
+      if (a==0) r=1; 
+      if (a==1) g=1;
+      if (a==2) b=1;
+      if (a==3) y=1;
     }
   }
 }
@@ -97,6 +141,10 @@ void pin_init(void) {
   gpio_set_dir(BTN_PIN_Y, GPIO_IN);
   gpio_pull_up(BTN_PIN_Y);
 
+  gpio_init(BTN_PIN_T);
+  gpio_set_dir(BTN_PIN_T, GPIO_IN);
+  gpio_pull_up(BTN_PIN_T);
+
   gpio_init(LED_PIN_B);
   gpio_set_dir(LED_PIN_B, GPIO_OUT);
 
@@ -112,3 +160,4 @@ void pin_init(void) {
   gpio_init(BUZZPIN);
   gpio_set_dir(BUZZPIN, GPIO_OUT);
 }
+
