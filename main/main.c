b1bdca5d4ea2ac6788eeeaec985f2dbaa9682a91
luiz <luiz@Ubuntu.myguest.virtualbox.org>
2024-03-03 19:34:15 -0300

    a

--- main/main.c
+++ main/main.c
@@ -14,6 +14,8 @@ const int LED_PIN_R = 8;
 const int LED_PIN_G = 14;
 const int LED_PIN_B = 11;
 const int LED_PIN_Y = 10;
+const int LED_PIN_I = 3;
+
 
 const int BUZZPIN = 6;
 
@@ -22,6 +24,9 @@ volatile int g=0;
 volatile int b=0;
 volatile int y=0;
 volatile int t=0;
+
+int ingame=0;
+
 
 void pin_init(void);
 
@@ -36,35 +41,88 @@ void som(int freq, int tempo, int pino){
   
 }
 
-// int random (int n) {
-//    int i;
-//    time_t t;
-//    int *vetor = (int *)malloc(n * sizeof(int));
-   
-//    /* Intializes random number generator */
-//    srand((unsigned) time(&t));
+void led (int pino, int freq){
+  gpio_put(pino, 1);
+  som(freq, 500, BUZZPIN);
+  gpio_put(pino, 0);
+  sleep_ms(200);
+}
 
-//    /* Print 5 random numbers from 0 to 49 */
-//    for( i = 0 ; i < n ; i++ ) {
-//       //printf("%d\n", rand() % 4);
-//       vetor[i]=rand() % 4;
-//    }
-   
-//    return(vetor);
-// }
+void gameover(int rodada){
+  printf("RODADA: %d", rodada);
+  som(3000, 500, BUZZPIN);
+  for (int i=0; i<rodada-1;i++){
+    gpio_put(LED_PIN_R,1);
+    gpio_put(LED_PIN_B,1);
+    gpio_put(LED_PIN_Y,1);
+    gpio_put(LED_PIN_G,1);
+    som(2000, 500, BUZZPIN);
+    gpio_put(LED_PIN_R,0);
+    gpio_put(LED_PIN_B,0);
+    gpio_put(LED_PIN_Y,0);
+    gpio_put(LED_PIN_G,0);
+    som(2000, 500, BUZZPIN);
+  }
+  gpio_put(LED_PIN_I, 1);
+
+
+}
+
+void confere(int botao, int v[], int *nc, int *rodada){
+
+  if (v[*nc]==botao){
+    *nc=*nc+1;
+  }else{
+    printf("ERROU\n");
+    *nc=0;
+    gameover(*rodada);
+    *rodada=1;
+    ingame=0;
+    
+  }
+
+}
+
+void sequencia (int n, int v[]) {
+   int i;
+   time_t t;
+   srand((unsigned) time(&t));
+   v[n-1]=rand() % 4;
+
+   for( i = 0 ; i < n ; i++ ) {
+      int a=v[i];
+      if (a==0) led(LED_PIN_R,1900);
+      if (a==1) led(LED_PIN_G,2000);
+      if (a==2) led(LED_PIN_B,2100);
+      if (a==3) led(LED_PIN_Y,2200);
+   }
+   ingame=1;
+   return ;
+}
+
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (gpio == BTN_PIN_R) r=1;
-  if (gpio == BTN_PIN_G) g=1;
-  if (gpio == BTN_PIN_B) b=1;
-  if (gpio == BTN_PIN_Y) y=1;
+  if (ingame){
+    if (gpio == BTN_PIN_R) r=1;
+    if (gpio == BTN_PIN_G) g=1;
+    if (gpio == BTN_PIN_B) b=1;
+    if (gpio == BTN_PIN_Y) y=1;
+  }
   if (gpio == BTN_PIN_T) t=1;
-
+  
 }
 
 int main() {
   stdio_init_all();
   pin_init();
+
+  int rodada=1;
+  int nc=0;
+  int vetor[100];
+
+  gpio_put(LED_PIN_I, 1);
+  
+  
 
   gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
@@ -76,50 +134,59 @@ int main() {
   while (true) {
     if (r){
       busy_wait_ms(jit);
-      printf("apertouR\n");
-      gpio_put(LED_PIN_R, 1);
-      som(1900, 300, BUZZPIN);
-      gpio_put(LED_PIN_R, 0);
+      if (ingame){
+        printf("apertouR\n");
+        led(LED_PIN_R,1900);
+        confere(0, vetor, &nc, &rodada);
+      }
       r=0;
     }
     if (g){
       busy_wait_ms(jit);
-      printf("apertouG\n");
-      gpio_put(LED_PIN_G, 1);
-      som(2000, 300, BUZZPIN);
-      gpio_put(LED_PIN_G, 0);
+      if (ingame==1){
+        printf("apertouG\n");
+        led(LED_PIN_G,2000);
+        confere(1, vetor, &nc, &rodada);
+      }
       g=0;
     }
     if (b){
       busy_wait_ms(jit);
-      printf("apertouB\n");
-      gpio_put(LED_PIN_B, 1);
-      som(2100, 300, BUZZPIN);
-      gpio_put(LED_PIN_B, 0);
+      if (ingame){
+        printf("apertouB\n");
+        led(LED_PIN_B,2100);
+        confere(2, vetor, &nc, &rodada);
+      }
       b=0;
     }
     if (y){
       busy_wait_ms(jit);
-      printf("apertouY\n");
-      gpio_put(LED_PIN_Y, 1);
-      som(2200, 300, BUZZPIN);
-      gpio_put(LED_PIN_Y, 0);
+      if (ingame){
+        printf("apertouY\n");
+        led(LED_PIN_Y, 2200);
+        confere(3, vetor, &nc, &rodada);
+      }
       y=0;
     }
+    if (nc==rodada){
+      rodada = rodada + 1;
+      nc=0;
+      ingame=0;
+      sleep_ms(150);
+      sequencia(rodada, vetor);
+    }
+
     if (t){
       busy_wait_ms(jit);
-      printf("apertouT\n");
-      int a;
-      int n=4;
-      //for( int i = 0 ; i < n ; i++ ) {
-        //printf("%d\n", rand() % 4);
-      //}
-      a=rand() % 4;
+      if (!ingame){
+        for (int i=0;i<4;i++){
+        led(LED_PIN_I, 2000);
+        }
+        sleep_ms(300);
+        printf("apertouT\n");
+        sequencia(rodada, vetor);
+      }
       t=0;
-      if (a==0) r=1; 
-      if (a==1) g=1;
-      if (a==2) b=1;
-      if (a==3) y=1;
     }
   }
 }
@@ -157,7 +224,9 @@ void pin_init(void) {
   gpio_init(LED_PIN_Y);
   gpio_set_dir(LED_PIN_Y, GPIO_OUT);
 
+  gpio_init(LED_PIN_I);
+  gpio_set_dir(LED_PIN_I, GPIO_OUT);
+
   gpio_init(BUZZPIN);
   gpio_set_dir(BUZZPIN, GPIO_OUT);
 }
-
