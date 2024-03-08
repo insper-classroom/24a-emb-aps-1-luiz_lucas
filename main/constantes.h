86feda46643d87f76bafdbe986123100da5ef398
luiz <luiz@Ubuntu.myguest.virtualbox.org>
2024-03-08 12:56:18 -0300

    a

--- main/constantes.h
+++ main/constantes.h
@@ -1,19 +1,5 @@
-
 #ifndef constantes
-#define constantes 
-
-
-// const int BTN_PIN_R = 8;
-// const int BTN_PIN_G = 6;
-// const int BTN_PIN_B = 22;
-// const int BTN_PIN_Y = 27;
-// const int BTN_PIN_T = 28;
-
-// const int LED_PIN_R = 9;
-// const int LED_PIN_G = 7;
-// const int LED_PIN_B = 21;
-// const int LED_PIN_Y = 26;
-// const int LED_PIN_I = 11;
+#define constantes
 
 const int BTN_PIN_R = 27;
 const int BTN_PIN_G = 26;
@@ -27,12 +13,9 @@ const int LED_PIN_B = 11;
 const int LED_PIN_Y = 10;
 const int LED_PIN_I = 5;
 
-const double jit=10;
+const double jit = 10;
 
 const int BUZZPIN = 20;
 const int BUZZPIN2 = 10;
 
 #endif
-
-
-
