6947d5e95c84c2c4f50e2eb9f46e3ace758c9d78
Lucas Lima <lucasfsl@al.insper.edu.br>
2024-03-08 20:09:55 +0000

    alterando portas

--- main/constantes.h
+++ main/constantes.h
@@ -1,21 +1,21 @@
 #ifndef CONSTANTES_H
 #define CONSTANTES_H
 
-#define BTN_PIN_R 27
-#define BTN_PIN_G 26
-#define BTN_PIN_B 18
-#define BTN_PIN_Y 22
-#define BTN_PIN_T 28
+#define BTN_PIN_R 4
+#define BTN_PIN_G 2
+#define BTN_PIN_B 26
+#define BTN_PIN_Y 28
+#define BTN_PIN_T 20
 
-#define LED_PIN_R 8
-#define LED_PIN_G 14
-#define LED_PIN_B 11
-#define LED_PIN_Y 10
-#define LED_PIN_I 5
+#define LED_PIN_R 5
+#define LED_PIN_G 3
+#define LED_PIN_B 22
+#define LED_PIN_Y 27
+#define LED_PIN_I 6
 
 #define jit 10
 
-#define BUZZPIN 20
-#define BUZZPIN2 10
+#define BUZZPIN 21
+#define BUZZPIN2 7
 
 #endif
